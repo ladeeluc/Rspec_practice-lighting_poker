65e3514c0e180ce6ba6905b8ea844d21a4a00a57
Ladeeluc <laura.hatch.lh@gmail.com>
2020-03-15 13:37:04 -0500

    update

--- Rspec_practice-lighting_poker/lib/card.rb
+++ Rspec_practice-lighting_poker/lib/card.rb
@@ -16,12 +16,11 @@ end
 def rank
     @rank
 end 
-
 end
 
 
 RSpec.describe 'a playing card' do
-    it 'has a suit'do
+    it 'has a suit' do
         raise unless Card.new(suit: :spades, rank: 4 ).suit == :spades 
     end
     it 'has a rank' do
