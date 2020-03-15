4995f8b65d4cedaafa67a117771be098b3bfd4fb
Ladeeluc <laura.hatch.lh@gmail.com>
2020-03-14 19:25:12 -0500

    updated files

---
+++ lib/card.rb
@@ -0,0 +1,21 @@
+class Card
+    def initialize(suit:, rank:)
+            @suit = suit
+            @rank = case rank
+            when  :jack then 11
+            when  :queen then 12
+            when  :king then 13
+            else rank
+            end
+     end      
+
+def suit
+     @suit
+end
+ 
+def rank
+    @rank
+end 
+
+end
+
